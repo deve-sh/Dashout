a14ac9a7ffbc12f352bdbdd21f81106675b14d36
Devesh Kumar <devesh2027@gmail.com>
2021-12-26 14:01:35 +0530

    Added auth listener for _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import auth from "../firebase/authentication";
+import request from "../helpers/request";
+import User from "../types/user";
+
+const AppWrapper = ({ Component, pageProps }) => {
+	useEffect(() => {
+		auth.onAuthStateChanged(async (user) => {
+			if (user) {
+				// User is signed in.
+				let displayName = user.displayName,
+					email = user.email,
+					emailVerified = user.emailVerified,
+					photoURL = user.photoURL,
+					phoneNumber = user.phoneNumber,
+					uid = user.uid,
+					providerData = user.providerData;
+				let userToSave: User = {
+					displayName,
+					email,
+					emailVerified,
+					photoURL,
+					phoneNumber,
+					uid,
+					providerInfo: JSON.parse(JSON.stringify(providerData)),
+				};
+				request({
+					endpoint: "/api/setupUser",
+					data: { user: userToSave },
+					requestType: "post",
+					options: {
+						headers: {
+							authentication: await auth.currentUser.getIdToken(false),
+						},
+					},
+				});
+			}
+		});
+	}, []);
+
+	return <Component {...pageProps} />;
+};
+
+export default AppWrapper;
