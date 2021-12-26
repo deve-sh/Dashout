d661c2ade03d20a966dcd46e5b018d019bf25ca8
Devesh Kumar <devesh2027@gmail.com>
2021-12-26 13:24:38 +0530

    Added connection caching to db connector

--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,21 @@
 import mysql from "mysql";
 
-var connection = mysql.createConnection(
-	process.env.PSCALE_DB_CONNECTION_STRING || {
-		host: process.env.PSCALE_DB_HOST,
-		user: process.env.PSCALE_DB_USERNAME,
-		password: process.env.PSCALE_DB_PASS,
-		database: process.env.PSCALE_DB_NAME,
+var connection;
+
+const connect = () => {
+	if (!connection) {
+		connection = mysql.createConnection(
+			process.env.PSCALE_DB_CONNECTION_STRING || {
+				host: process.env.PSCALE_DB_HOST,
+				user: process.env.PSCALE_DB_USERNAME,
+				password: process.env.PSCALE_DB_PASS,
+				database: process.env.PSCALE_DB_NAME,
+			}
+		);
+		connection.connect();
 	}
-);
 
-connection.connect();
+	return connection;
+};
 
-export default connection;
+export default connect();
