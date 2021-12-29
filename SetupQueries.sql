4f490eb0e343863e45aceec8be3db7569542ebc8
Devesh Kumar <devesh2027@gmail.com>
2021-12-29 09:35:12 +0530

    Added SQL Setup Query for merchants table

--- SetupQueries.sql
+++ SetupQueries.sql
@@ -6,5 +6,21 @@ CREATE TABLE "users" (
     "phone_number" varchar(255) UNIQUE,
     "photo_url" varchar(255) DEFAULT NULL,
     "provider_info" text,
-    "disabled" boolean DEFAULT 0
+    "disabled" boolean DEFAULT 0,
+    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP(),
+    "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP()
+);
+
+CREATE TABLE "merchants" (
+    "id" varchar(75) NOT NULL PRIMARY KEY,
+    "email" varchar(255) NOT NULL,
+    "email_verified" boolean DEFAULT 0,
+    "name" varchar(255),
+    "phone_number" varchar(255) UNIQUE NOT NULL,
+    "description" text DEFAULT NULL,
+    "photo_url" varchar(255) DEFAULT NULL,
+    "client_id" varchar(255) NOT NULL UNIQUE,
+    "client_secret" varchar(255) NOT NULL UNIQUE,
+    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP(),
+    "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP()
 );
