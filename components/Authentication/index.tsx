fc452e53fef424928634a0e4ec7fa8b73e305969
Devesh Kumar <devesh2027@gmail.com>
2022-01-01 16:54:47 +0530

    Updated Authentication component to contain Github login

--- components/Authentication/index.tsx
+++ components/Authentication/index.tsx
@@ -1,7 +1,8 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { Button } from "@chakra-ui/react";
 
 import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 import { loginWithGoogle, loginWithGithub } from "../../API/auth";
 import toasts from "../../helpers/toasts";
@@ -32,6 +33,18 @@ const Authentication = () => {
 				size="lg"
 			>
 				Sign In With Google
+			</Button>
+			<Button
+				isLoading={isLoggingIn}
+				isFullWidth
+				variant="outline"
+				colorScheme="gray"
+				leftIcon={<FaGithub size="1.5rem" />}
+				onClick={() => signInUser("github")}
+				size="lg"
+				marginTop="15px"
+			>
+				Sign In With Github
 			</Button>
 		</>
 	);
