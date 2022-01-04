57df9f5bb9248146db5806e7a6309b077bed3bab
Devesh Kumar <devesh2027@gmail.com>
2022-01-04 20:01:17 +0530

    Added orders table SQL Schema

--- SetupQueries.sql
+++ SetupQueries.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "users" (
+CREATE TABLE IF NOT EXISTS "users" (
     "uid" varchar(75) NOT NULL PRIMARY KEY,
     "email" varchar(255) NOT NULL,
     "email_verified" boolean DEFAULT 0,
@@ -13,11 +13,11 @@ CREATE TABLE "users" (
     "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP()
 );
 
-CREATE TABLE "merchants" (
+CREATE TABLE IF NOT EXISTS "merchants" (
     "id" varchar(75) NOT NULL PRIMARY KEY,
     "email" varchar(255) NOT NULL,
     "name" varchar(255),
-    "uid" varchar(255) NOT NULL references users(uid) ON DELETE CASCADE ON UPDATE
+    "user" varchar(255) NOT NULL references users(uid) ON DELETE CASCADE ON UPDATE
     SET
         NULL,
         "phone_number" varchar(255) UNIQUE NOT NULL,
@@ -29,3 +29,22 @@ CREATE TABLE "merchants" (
         "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP(),
         "n_transactions" integer DEFAULT 0
 );
+
+CREATE TABLE IF NOT EXISTS "orders" (
+    "id" varchar(75) NOT NULL PRIMARY KEY,
+    "title" varchar(255),
+    "description" text DEFAULT "",
+    "merchant" varchar(255) NOT NULL references merchants(id) ON DELETE CASCADE ON UPDATE
+    SET
+        NULL,
+        "item_id" varchar(255) NOT NULL,
+        "item_name" varchar(255) NOT NULL,
+        "item_description" varchar(255),
+        "price_per_unit" float,
+        "quantity" float,
+        "currency" varchar(5) DEFAULT "INR",
+        "paid" boolean DEFAULT 0,
+        "paid_at" timestamp DEFAULT NULL,
+        "created_at" timestamp DEFAULT CURRENT_TIMESTAMP(),
+        "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP(),
+)
