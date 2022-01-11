9108c37ec5bb7694bc7cb6ab7755e82aec89604f
Devesh Kumar <devesh2027@gmail.com>
2022-01-11 17:48:34 +0530

    Fixed Firebase Admin error

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -2,6 +2,8 @@ import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
 if (!admin.apps.length) {
 	admin.initializeApp({
 		credential: admin.credential.cert(serviceAccount),
