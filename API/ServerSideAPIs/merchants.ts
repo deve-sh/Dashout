1297bb13546565b6fd8c7c682cdaa1f25a3b8b40
Devesh Kumar <devesh2027@gmail.com>
2022-01-23 18:03:02 +0530

    Added base setup for server side API Functions

---
+++ API/ServerSideAPIs/merchants.ts
@@ -0,0 +1,3 @@
+import admin from "../../firebase/admin";
+
+// Create, fetch and manipulate merchants here.
---
+++ API/ServerSideAPIs/transactions.ts
@@ -0,0 +1,3 @@
+import admin from "../../firebase/admin";
+
+// Create and manipulate user transactions here.
