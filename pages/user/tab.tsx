3ab66c19a4fb11bd84f365ab30f7812bc6190f4f
Devesh Kumar <devesh2027@gmail.com>
2022-01-23 17:58:12 +0530

    Setup basic transactions fetcher and stat population on user's tab

---
+++ pages/user/tab.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useRef, useState } from "react";
+import Head from "next/head";
+import styled from "@emotion/styled";
+
+import { Stat, StatLabel, StatNumber, StatGroup } from "@chakra-ui/react";
+
+import setupProtectedRoute from "../../helpers/setupProtectedRoute";
+import ContentWrapper from "../../components/Layout/ContentWrapper";
+
+import useUser from "../../hooks/useUser";
+import User from "../../types/user";
+import { getUserTransactions } from "../../API/transactions";
+import toasts from "../../helpers/toasts";
+
+const UserTab = () => {
+	const user = useUser() || ({} as User);
+
+	const [userTransactions, setUserTransactions] = useState([]);
+	const [hasMoreTransactions, setHasMoreTransactions] = useState(true);
+	const userTransactionsStartAfter = useRef(null);
+
+	const fetchUserTransactions = async () => {
+		if (!user || !user.uid || !hasMoreTransactions) return;
+		getUserTransactions(
+			user.uid,
+			userTransactionsStartAfter.current,
+			(err, transactionsList) => {
+				if (err) return toasts.error(err);
+				setUserTransactions(
+					transactionsList.map((transactionDoc) => transactionDoc.data())
+				);
+				userTransactionsStartAfter.current =
+					transactionsList[transactionsList.length - 1];
+				setHasMoreTransactions(transactionsList.length >= 10);
+			}
+		);
+	};
+
+	useEffect(() => {
+		fetchUserTransactions();
+	}, [user.uid]);
+
+	return (
+		<>
+			<Head>
+				<title>Dashout - {user.displayName || "User"}'s Tab</title>
+			</Head>
+			<ContentWrapper>
+				<StatGroup
+					marginTop="var(--standard-spacing)"
+					border="0.01rem solid var(--bordergrey)"
+					borderRadius="0.25rem"
+					padding="var(--standard-spacing)"
+				>
+					<Stat>
+						<StatLabel>Transactions</StatLabel>
+						<StatNumber>{user.nTransactions || 0}</StatNumber>
+					</Stat>
+					<Stat>
+						<StatLabel>Transaction Amount</StatLabel>
+						<StatNumber>₹{user.totalTransactionAmount || 0}</StatNumber>
+					</Stat>
+					<Stat>
+						<StatLabel>Amount Repaid</StatLabel>
+						<StatNumber>₹{user.totalAmountRepaid || 0}</StatNumber>
+					</Stat>
+				</StatGroup>
+			</ContentWrapper>
+		</>
+	);
+};
+
+UserTab.getInitialProps = setupProtectedRoute();
+
+export default UserTab;
