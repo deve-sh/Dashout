b522f7ebfedfa422e45b20ddf946fc8feb839844
Devesh Kumar <devesh2027@gmail.com>
2022-01-24 20:40:29 +0530

    Added getMerchant Server Side API Func

--- API/ServerSideAPIs/merchants.ts
+++ API/ServerSideAPIs/merchants.ts
@@ -1,3 +1,20 @@
 import admin from "../../firebase/admin";
 
 // Create, fetch and manipulate merchants here.
+export const getMerchant = async (clientId: string, clientSecret: string) => {
+	try {
+		const merchant = (
+			await admin
+				.firestore()
+				.collection("merchants")
+				.where("clientId", "==", clientId)
+				.where("clientSecret", "==", clientSecret)
+				.limit(1)
+				.get()
+		).docs[0];
+		return merchant || null;
+	} catch (err) {
+		console.log(err);
+		return null;
+	}
+};
