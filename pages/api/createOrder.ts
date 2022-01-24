4cc4b6c2fd96ebcebf369a142c7350e3c51363ff
Devesh Kumar <devesh2027@gmail.com>
2022-01-24 20:46:41 +0530

    Added createOrder API Route

---
+++ pages/api/createOrder.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { OrderItem } from "../../types/order";
+
+import { getMerchant } from "../../API/ServerSideAPIs/merchants";
+import { createNewOrder } from "../../API/ServerSideAPIs/orders";
+
+export default async function createOrder(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const error = (status: number, message: string) =>
+		res.status(status).json({
+			error: message,
+			message,
+		});
+
+	try {
+		const { authorization } = req.headers;
+		const { item } = req.body as { item: OrderItem };
+
+		const clientId = authorization.split("-")[0];
+		const clientSecret = authorization.split("-")[1];
+
+		if (!clientId || !clientSecret) return error(403, "Invalid credentials.");
+		if (!item) return error(400, "Incomplete Information");
+
+		const merchant = await getMerchant(clientId, clientSecret);
+		if (!merchant) return error(403, "Invalid credentials");
+
+		await createNewOrder(merchant.id, item, (err, createdOrder) => {
+			if (err) return error(500, "Failed to create order.");
+			else
+				res.status(201).json({
+					message: "Created order successfully.",
+					order: createdOrder,
+				});
+		});
+	} catch (err) {
+		return error(500, err.message);
+	}
+}
